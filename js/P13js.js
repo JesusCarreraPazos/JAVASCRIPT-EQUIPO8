ac5370a7b07896c21b6380754d07f9312ae6b31a
JesusCarreraPazos <162249173+JesusCarreraPazos@users.noreply.github.com>
2024-05-02 11:12:24 -0600

    Add files via upload

---
+++ js/P13js.js
@@ -0,0 +1,15 @@
+function agregarLinea() {
+    var oTbl = document.getElementById("tblUno");
+    var oLin = oTbl.insertRow(-1);
+    var oCelda1 = oLin.insertCell(0);
+    var oCelda2 = oLin.insertCell(1);
+    var oCelda3 = oLin.insertCell(2);
+
+    oCelda1.innerHTML = "Celda";
+    oCelda2.innerHTML = 'Texto y campo ' + '<input type="text" name="campo"/>';
+    oCelda3.innerHTML = "agregado"
+}
+ function eliminarLinea() {
+    var oTbl = document.getElementById("tblUno");
+    oTbl.deleteRow(-1);
+ }
